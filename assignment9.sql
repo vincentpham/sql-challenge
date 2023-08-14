9ec3d33a103a1725ed120dc3d06505a2af44ca73
vincentpham <56554113+vincentpham@users.noreply.github.com>
2023-08-14 18:53:30 -0400

    just the base schema start

---
+++ assignment9.sql
@@ -0,0 +1,66 @@
+-- Database: assignment_9
+
+-- DROP DATABASE IF EXISTS assignment_9;
+
+/*CREATE DATABASE assignment_9
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Canada.1252'
+    LC_CTYPE = 'English_Canada.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+*/	
+-- creating tables
+
+-- titles table
+
+CREATE TABLE Titles (
+    title_id VARCHAR PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+-- departments table
+
+CREATE TABLE Departments (
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
+);
+
+-- employees table 
+
+CREATE TABLE Employees (
+    emp_no INTEGER PRIMARY KEY,
+    emp_title_id VARCHAR REFERENCES Titles(title_id),
+    birth_date DATE,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex CHAR,
+    hire_date DATE
+);
+
+-- salaries
+
+CREATE TABLE Salaries (
+    emp_no INTEGER REFERENCES Employees(emp_no),
+    salary INTEGER NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+-- dept_emp
+
+CREATE TABLE Dept_Emp (
+    emp_no INTEGER REFERENCES Employees(emp_no),
+    dept_no VARCHAR REFERENCES Departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+-- dept_manager table
+
+CREATE TABLE Dept_Manager (
+    emp_no INTEGER REFERENCES Employees(emp_no),
+    dept_no VARCHAR REFERENCES Departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
